303c3d6d930b0558cc7c6a9f8541365a9bb13507
Sreenath285 <sreenath.285@gmail.com>
2022-04-14 12:05:54 +0530

    View employee payroll details from local storage

---
+++ EmployeePayrollApp/js/HomePage.js
@@ -0,0 +1,43 @@
+let employeePayrollList;
+window.addEventListener("DOMContentLoaded", (event) => {
+  employeePayrollList = getEmployeePayrollDataFromStorage();
+  document.querySelector(".emp-count").textContent = employeePayrollList.length;
+  createInnerHtml();
+  localStorage.removeItem('editEmp');
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+  return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+const createInnerHtml = () => {
+  const headerHtml =
+    "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+  if (employeePayrollList.length == 0) return;
+  let innerHtml = `${headerHtml}`;
+  for (const employeePayrollData of employeePayrollList) {
+    innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" src="${employeePayrollData._profileImage}" alt="profile_img-1"></td>
+        <td>${employeePayrollData._name}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td>${getDepartmentHtml(employeePayrollData._department)}</td>
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
+        <td>
+          <img name="${employeePayrollData._id}" src="..\assets\icons\delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+          <img name="${employeePayrollData._id}" src="..\assets\icons\create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+        </td>
+      </tr>`; 
+  }                  
+  document.querySelector("#table-display").innerHTML = innerHtml;
+};
+
+const getDepartmentHtml = (deptList) => {
+ let deptHtml = '';
+  for (const dept of deptList) {
+   deptHtml = `${deptHtml}<div class='dept-label'>${dept}</div>`
+  }
+ return deptHtml;
+}
